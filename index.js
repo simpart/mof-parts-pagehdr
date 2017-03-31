71d345b5d215396c0bc70d20a5a1b8bbe68fda55
Your Name <you@example.com>
2017-03-31 10:36:09 +0900

    refactoring

--- index.js
+++ index.js
@@ -7,61 +7,59 @@ require('mofron-comp-text');
 require('mofron-event-click');
 
 mofron.comp.header.Title = class extends mofron.comp.Header {
-    
-    constructor (prm_opt) {
+    initDomConts(prm) {
         try {
-            super();
-            this.name("Title");
+            super.initDomConts();
+            this.name('Title');
             
-            this.m_url = null;
+            /* set header style */
+            this.style({
+                'display'     : 'flex',
+                'align-items' : 'center'
+            });
             
-            this.prmOpt(prm_opt);
+            /* set header title */
+            this.addChild(this.title());
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    initDomConts(prm) {
+    title (val) {
         try {
-            super.initDomConts();
-            
-            /* check parameter */
-            if ('string' != (typeof prm)) {
-                throw new Error('invalid parameter type');
-            }
-            
-            /* set header style */
-            this.style('display'    , 'flex');
-            this.style('align-items', 'center');
-            
-            /* set header title */
-            var title = new mofron.comp.Text({
-                            param : prm,
-                            size  : 35,
-                            style : new mofron.Param('margin-left', '20px'),
-                            event : new mofron.event.Click(
-                                        function (ttl) {
-                                            try {
-                                                var url = ttl.url();
-                                                if (null !== url) {
-                                                    location.href = url;
-                                                }
-                                            } catch (e) {
-                                                console.log(e.stack);
-                                            }
-                                        },
-                                        this
-                                    )
-                        });
-            this.addChild(title);
-            
-            var clr = this.color();
-            if (null !== clr) {
-                var rgb = this.color().rgba();
-                if (290 > (rgb[0]+rgb[1]+rgb[2])) {
-                    title.color(new mofron.Color(255,255,255));
+            if (undefined === val) {
+                /* getter */
+                if (undefined === this.m_title) {
+                    this.title(new mofron.comp.Text(''));
                 }
+                return this.m_title;
+            }
+            /* setter */
+            if (true === mofron.func.isInclude(val, 'Text')) {
+                val.size((null === val.size()) ? 35 : undefined);
+                val.style({ 'margin-left' : '20px' });
+                this.m_title = val;
+            } else if ('string' === typeof val) {
+                this.title().text(val);
+            } else {
+                throw new Error('invalid parameter');
+            }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    color (clr) {
+        try {
+            var ret = super.color(clr);
+            if (undefined !== ret) {
+                return ret;
+            }
+            var rgb = clr.rgba();
+            if (290 > (rgb[0]+rgb[1]+rgb[2])) {
+                this.title().color(new mofron.Color(255,255,255));
             }
         } catch (e) {
             console.error(e.stack);
@@ -73,11 +71,27 @@ mofron.comp.header.Title = class extends mofron.comp.Header {
         try {
             if (undefined === ul) {
                 /* getter */
-                return this.m_url;
+                return (undefined === this.m_url) ? null : this.m_url;
             }
             /* setter */
             if ('string' !== typeof ul) {
                 throw new Error('invalid prameter');
+            }
+            if (undefined === this.m_url) {
+                this.title().addEvent(
+                    new mofron.event.Click(
+                        (ttl) => {
+                            try {
+                                if (null !== ttl.url()) {
+                                    location.href = ttl.url();
+                                }
+                            } catch (e) {
+                                console.log(e.stack);
+                            }
+                        },
+                        this
+                    )
+                );
             }
             this.m_url = ul;
         } catch (e) {
